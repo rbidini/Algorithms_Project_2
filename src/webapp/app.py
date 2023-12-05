23ce6cd366bbf3c27281ea0a7d2593f4f56ee7eb
anutabutsko <anutabutsko@gmail.com>
2023-12-05 14:22:50 -0500

    added comments to flask app

--- src/webapp/app.py
+++ src/webapp/app.py
@@ -1,26 +1,32 @@
 from flask import Flask, render_template, request
+
 from src.main import run_algorithm
 
 app = Flask(__name__)
 
 
+# Home page
 @app.route("/", methods=["GET"])
 def home():
     # Render the home page for GET requests
     return render_template("home.html")  # home.html is the page with the input form
 
 
+# Results page
 @app.route("/result", methods=["POST"])
 def result():
     source = request.form['source']
     destination = request.form['destination']
 
+    # If the user entered the same city for both source and destination
     if source == destination:
         return render_template("home.html", error_message="Source and destination cities cannot be the same.")
 
+    # Check if both source and destination inputs are given
     if source and destination:
         result, max_capacity = run_algorithm(source, destination)
 
+        # In case source, destination, or both are missing, render the home page with an error message
         if not result:
             # Handle error messages
             if max_capacity == 'source and destination':
@@ -33,14 +39,17 @@ def result():
             # Redirect back to the home page with the error message
             return render_template("home.html", error_message=error_message)
 
+        # Sort by capacity in decreasing order
         result = sorted(result, key=lambda x: x["maximum capacity"], reverse=True)
 
         # Render the results page with the display results
-        return render_template("results.html", display_results=result, source_city=source.title(), destination_city=destination.title(), max_capacity=max_capacity)
+        return render_template("results.html", display_results=result, source_city=source.title(),
+                               destination_city=destination.title(), max_capacity=max_capacity)
 
     # In case source or destination is missing, render the home page again with an error message
     return render_template("home.html", error_message="Please enter both source and destination.")
 
 
+# Run the app
 if __name__ == "__main__":
     app.run(debug=True)
